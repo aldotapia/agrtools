ea033f046a5bc7eace365704ac5babf8d7f4c348
aldotapia <aatapia@userena.cl>
2024-03-25 18:28:48 -0300

    Function fix

--- R/chillinghours.R
+++ R/chillinghours.R
@@ -6,20 +6,30 @@
 #' @param threshold numeric. A vector of two values representing the lower and upper threshold temperatures.
 #' @param tunit character. The unit of the temperature data. Default is "celsius".
 #' @param thresholdunit character. The unit of the threshold temperature. Default is "celsius".
-#' @param output character. The type of output. Default is "chillhours".
+#' @param method character. The method to compute chilling hours. Default is "classic".
+#' @param output character. The type of output, only valid for method = "classic". Default is "chillhours".
 #' @param na.rm logical. Should missing values be removed? Default is TRUE.
 #'
 #' @return A numeric value representing the chilling hours or chilling units.
 #'
 #' @examples
-#' chillinghours(c(5, 6, 7, 8, 9, 10))
+#' chillinghours(c(5, 6, 7, 8, 15, 18))
+#' chillinghours(c(5, 6, 7, 8, 15, 18), method = "utah")
+#' chillinghours(c(5, 6, 7, 8, 15, 18), method = "dpcu")
 #'
 #' @export
 chillinghours = function(t_h, threshold = c(0,7.2),
                          tunit = "celsius",
                          thresholdunit = "celsius",
+                         method = c("classic","utah","dpcu"),
                          output = c("chillhours", "chillunits"),
                          na.rm = TRUE) {
+  temperature_list = c("celsius", "fahrenheit", "kelvin")
+  method_list = c("classic", "utah", "dpcu")
+  rc = data.frame('temperature' = c(1.4,2.4,9.1,12.4,15.9,17.9),
+                  'unit' = c(0.5, 1, 0.5, 0, -0.5, -1))
+  rc2 = data.frame('temperature' = c(1.4,2.4,9.1,12.4,15.9,17.9),
+                  'unit' = c(0.5, 1, 0.5,0, -0.5, 0))
   if(na.rm){
     t_h = t_h[!is.na(t_h)]
   }
@@ -29,7 +39,6 @@ chillinghours = function(t_h, threshold = c(0,7.2),
   if(length(threshold) != 2){
     stop("threshold should have two values")
   }
-  temperature_list = c("celsius", "fahrenheit", "kelvin")
   if (!tunit %in% temperature_list) {
     stop("tunit should be one of celsius, fahrenheit, kelvin")
   }
@@ -39,15 +48,34 @@ chillinghours = function(t_h, threshold = c(0,7.2),
   if (tunit != thresholdunit) {
     stop("tunit and thresholdunit should be the same")
   }
-
-  output = match.arg(output)
-  if(sum(output %in% c("chillhours", "chillunits"))==0){
-    stop("output should be one of chillhours, chillunits")
+  method = match.arg(method)
+  if (!method %in% method_list) {
+    stop("method should be one of classic, utah")
   }
-
-  switch (output,
-    'chillhours' = sum((t_h < threshold[2]) & (t_h > threshold[1])),
-    'chillunits' = sum(threshold[2] - t_h[(t_h < threshold[2]) &
-                                            (t_h > threshold[1])])
-  )
+  if(method == "classic"){
+    output = match.arg(output)
+    if(sum(output %in% c("chillhours", "chillunits"))==0){
+      stop("output should be one of chillhours, chillunits")
+    }
+    if(output == "chillhours"){
+      t_h = sum((t_h < threshold[2]) & (t_h > threshold[1]))
+    }else{
+      t_h = sum(threshold[2] - t_h[(t_h < threshold[2]) & (t_h > threshold[1])])
+      }
+  } else if(method == "utah"){
+    if(tunit == "celsius"){
+      t_h = cut(t_h, breaks = c(-Inf, rc$temperature, Inf),
+                labels = c(0, rc$unit))
+      t_h = as.numeric(as.character(t_h))
+      t_h = sum(t_h)
+    }
+  }else if(method == "dpcu"){
+    t_h = cut(t_h, breaks = c(-Inf, rc2$temperature, Inf),
+              labels = c(0, rc2$unit))
+    t_h = as.numeric(as.character(t_h))
+    t_h = sum(t_h)
+  }else{
+    stop("For utah method, the temperature should be in celsius")
+  }
+  return(t_h)
 }
